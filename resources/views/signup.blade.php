d62ccb3a71d2e1c721c5ace337340d9df4e5b68c
Daniele Severiano <danisck@gmail.com>
2022-10-09 02:44:04 -0300

    Layout da página Cadastro

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,38 +1,60 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cadastrar</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 </head>
 
-<body>
-    <h1>Cadastre-se</h1>
-    <form action="{{ route('register') }}" method="post">
-        @csrf
-        <label for="name">Nome Completo</label>
-        <input type="text" name="name" placeholder="Nome completo">
-        <label for="email">E-mail</label>
-        <input type="email" name="email" placeholder="E-mail">
-        <label for="password">Senha</label>
-        <input type="password" name="password" placeholder="Senha">
-        <input type="submit" value="Cadastrar">
-    </form>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    @if (session('danger'))
-        <p>{{ session('danger') }}</p>
-    @endif
-    <p>Já possui uma conta? Faça seu <a href="{{ route('login') }}">Login</a>!</p>
+<body class="signup">
+    <section class="container text-center col-sm-4 mb-3" id="box">
+        <h1 class="mb-3">Cadastre-se</h1>
+        <form action="{{ route('register') }}" method="POST" class="column justify-content-md-center mb-3" novalidate>
+            @csrf
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingPassword" name="name" placeholder="Password"
+                    required>
+                <label for="floatingPassword">Digite seu nome</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" name="email"
+                    placeholder="name@example.com" required>
+                <label for="floatingInput">Digite seu e-mail</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password"
+                    required>
+                <label for="floatingPassword">Digite sua senha</label>
+            </div>
+            <div class="col-12">
+                <button class="btn btn-primary" type="submit">Cadastrar</button>
+            </div>
+        </form>
+        @if ($errors->any())
+            <div class="alert alert-danger alert-dismissible fade show position-absolute top-50 start-50 translate-middle">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li style="list-style: none;">{{ $error }}</li>
+                    @endforeach
+                </ul>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+        @if (session('danger'))
+            <p>{{ session('danger') }}</p>
+        @endif
+        <p>Já possui uma conta? Faça seu <a href="{{ route('login') }}">Login</a>!</p>
+    </section>
 </body>
 
 </html>
