fe068431160d9ed99cd6be4a5944ae5d1c0deba5
Daniele Severiano <danisck@gmail.com>
2022-10-08 21:51:35 -0300

    Adição de menu e footer

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</head>
+
+<body class="position-relative">
+    @yield('header')
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <a href="/" class="navbar-brand">
+                    <img src="./img/logo.svg" alt="Logo" style="width: 50px">
+                    Convert
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a href="{{ route('index') }}" class="nav-link">Home</a>
+                        </li>
+                        @if (!Auth::check())
+                            <li class="nav-item">
+                                <a href="{{ route('login') }}" class="nav-link">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('signup') }}" class="nav-link">Cadastro</a>
+                            </li>
+                        @endif
+                        @auth
+                            <li class="nav-item">
+                                <a href="{{ route('create') }}" class="nav-link">Dashboard</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Olá, {{ Auth::user()->name }}!
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="{{ route('edit') }}">Edite seus dados</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="{{ route('logout') }}">Logout</a></li>
+                                </ul>
+                            @endauth
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    @yield('content')
+    @if (session('msg'))
+        <div class="alert alert-success alert-dismissible fade show position-fixed top-0 start-0" role="alert">
+            <strong>{{ session('msg') }}</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+    <footer>
+        <p>Desenvolvido por <strong><a href="https://github.com/daniseveriano/conversor-de-moedas" target="_blank"
+                    noopener noreferrer>Dani Severiano</a></strong> - © Todos os direitos reservados</p>
+    </footer>
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.main')
+@section('title', 'Conversor de Moedas')
+@section('header')
+@section('content')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+    <body class="col-md-12">
+        <p>Seja bem-vindo ao nosso sistema de conversão de moedas!</p>
+        <p>Conversão entre BRL, CAD e USD em tempo real!</p>
+        <p>Cadastre-se para ter acesso a todas as nossas funcionalidades!</p>
+    </body>
 
-<body>
-    @auth
-        <h3>O usuário logado é {{ Auth::user()->name }}</h3>
-    @endauth
-    <p>Seja bem-vindo ao nosso sistema de conversão de moedas!</p>
-    <p>Conversão entre BRL, CAD e USD em tempo real!</p>
-    <p>Cadastre-se para ter acesso a todas as nossas funcionalidades!</p>
-    @if (!Auth::check())
-        <a href="{{ route('login') }}">Login</a>
-        <a href="{{ route('signup') }}">Cadastro</a>
-    @endif
-    @auth
-        <a href="{{ route('create') }}">Dashboard</a>
-        <a href="{{ route('edit') }}">Edite seus dados</a>
-        <a href="{{ route('logout') }}">Logout</a>
-    @endauth
-</body>
-
-</html>
+@endsection
