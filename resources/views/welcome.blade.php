7d0302583430cc5e302cad51728e153b7178f3df
Daniele Severiano <danisck@gmail.com>
2022-10-08 23:59:30 -0300

    Layout da página Home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,20 @@
 @section('content')
 
     <body class="col-md-12">
-        <p>Seja bem-vindo ao nosso sistema de conversão de moedas!</p>
-        <p>Conversão entre BRL, CAD e USD em tempo real!</p>
-        <p>Cadastre-se para ter acesso a todas as nossas funcionalidades!</p>
-    </body>
+        <section>
+            <div id="banner" class="col-md-12">
+                <h1>Taxa de câmbio em tempo real</h1>
+                <h1>com precisão e facilidade</h1>
+                <br>
+                <h5>Conversão de moedas de forma fácil, com acesso a</h5>
+                <h5>históricos de conversões para consultas futuras</h5>
+                <h5>Dados atualizados em tempo real</h5>
+            </div>
+            <div class="grid text-center">
+                <h5>Cadastre-se e comece agora!</h5>
+                <a type="button" class="btn btn-primary btn-lg" href="/signup">Cadastrar</a>
+                <p style="margin-top: 30px; margin-bottom: 30px;">Já possui uma conta? Faça seu <a href="/login">Login</a>!</p>
+            </div>
+        </section>
 
-@endsection
+    @endsection
