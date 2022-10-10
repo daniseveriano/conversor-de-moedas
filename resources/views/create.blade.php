8522218b7481a55aceb5de1279d470b105cea46e
Daniele Severiano <danisck@gmail.com>
2022-10-10 01:47:59 -0300

    Reversão de modificações

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -145,6 +145,6 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
-    </body>
+    </body>.
 
 @endsection
